39a1739d125bd08c8d3a302177602ed74c22123f
Your Name <you@example.com>
2023-10-27 12:54:56 +0300

    1

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int x;
+    int b = 0;
+
+    cin >> x;
+
+    while (x)
+    {
+        x = x / 10;
+        b++;
+    }
+    cout << endl << b;
+}
---
+++ 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    setlocale(0, "");
+    const int A = 10;
+    int mas[A];
+    cout << "Введите элементы массива: \n";
+    for (int i = 0; i != 10; i++)
+    {
+        cin >> mas[i];
+    }
+    for (int i = 0; i != 10; i++)
+    {
+
+        x += mas[i];
+    }
+    
+    cout << x;
+}
